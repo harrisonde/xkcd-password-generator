06cca0b3e64e2011472059f5ed59f482791f19b3
Harrison DeStefano <harrison.destefano@gmail.com>
2014-07-07 14:42:51 -0400

    removed error log from logic.php

--- functions/logic.php
+++ functions/logic.php
@@ -12,10 +12,6 @@
 	 *
 	 */
 	 
-	 // uncomment for development log - do not use in a production environment.
-	 //error_reporting(E_ALL);       # Report Errors, Warnings, and Notices
-	 //ini_set('display_errors', 1); # Display errors on page (instead of a log file)
-	 
 	 /* 
 	  *	Associative arrays are arrays that use named keys that you assign to them. Any data provided by the HTTP 
 	  *	POST aka user is an associative array.
